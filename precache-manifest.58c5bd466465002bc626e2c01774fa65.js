a774488660f32263420ed19f4e9c77a91430f561
khantmawhein <kokhant228abc@gmail.com>
2020-06-17 10:02:54 +0630

    Updates

---
+++ precache-manifest.58c5bd466465002bc626e2c01774fa65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28c3f7dbb38caa7ff9b030d94bb3647",
+    "url": "/my-counter-app/index.html"
+  },
+  {
+    "revision": "562c51de430d7b45ceec",
+    "url": "/my-counter-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "96faa1ec4f2144ef83da",
+    "url": "/my-counter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "562c51de430d7b45ceec",
+    "url": "/my-counter-app/static/js/2.3d4c12ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-counter-app/static/js/2.3d4c12ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96faa1ec4f2144ef83da",
+    "url": "/my-counter-app/static/js/main.8a0774e1.chunk.js"
+  },
+  {
+    "revision": "837728c27ec15fc380d7",
+    "url": "/my-counter-app/static/js/runtime-main.b7c5bd66.js"
+  }
+]);
